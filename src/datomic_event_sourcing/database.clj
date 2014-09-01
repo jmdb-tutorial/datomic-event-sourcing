31bf25eb68ff5ec278905b459d11be64545b7f67
Jim Barritt <jim@planet-ix.com>
2014-09-01 13:59:57 +0100

    [jmdb] Refactoring to pull out util methods

--- src/datomic_event_sourcing/database.clj
+++ src/datomic_event_sourcing/database.clj
@@ -1,5 +1,6 @@
 (ns datomic-event-sourcing.database
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [datomic-event-sourcing.util :as util]))
 
 
 (def uri "datomic:mem://customers")
@@ -16,17 +17,9 @@
 ;;(def data-tx (read-string (slurp "db/sample-data.dtm")))
 ;;@(d/transact conn data-tx)
 
-(defn get-inserted-entity
-  [tempid tx-res]
-  (d/entity (:db-after tx-res) (d/resolve-tempid (:db-after tx-res) (:tempids tx-res) tempid)))
-
-(defn transact-entity
-  [conn tempid tx]
-  (get-inserted-entity tempid @(d/transact conn tx)))
-
 (let [tempid (d/tempid :db.part/user)]
-  (def customer
-    (transact-entity
+  (def customer-bob
+    (util/transact-entity
      conn
      tempid
      [[:db/add tempid :customer/email "bob@foo.com"]
@@ -35,55 +28,20 @@
       [:db/add tempid :customer/address-town "Some Town"]
       [:db/add tempid :customer/address-postcode "RE45 2WE"]])))
 
-(def entity-id (:db/id customer))
+(def customer-bob-id (:db/id customer-bob))
 
-(transact-entity
+(util/transact-entity
  conn
- entity-id
- [[:db/add entity-id :customer/address-line-1 "2 Some New Street"]
-  [:db/add entity-id :customer/address-town "Some New Town"]
-  [:db/add entity-id :customer/address-postcode "GH45 2SD"]])
+ customer-bob-id
+ [[:db/add customer-bob-id :customer/address-line-1 "2 Some New Street"]
+  [:db/add customer-bob-id :customer/address-town "Some New Town"]
+  [:db/add customer-bob-id :customer/address-postcode "GH45 2SD"]])
 
 (def db (d/db conn))
 
 
-(->>
- ;; This query finds all tuples of the tx and the actual attribute that
- ;; changed for a specific entity.
- (d/q
-  '[:find ?tx ?a
-    :in $ ?e
-    :where
-    [?e ?a _ ?tx]]
-  (d/history db)
-  entity-id)
- ;; We group the tuples by tx - a tx single can and will contain multiple
- ;; attribute changes.
- (group-by (fn [[tx attr]] tx))
- ;; We only want the actual changes
- (vals)
- ;; Sort with oldest first
- (sort-by (fn [[tx attr]] tx))
- ;; Generate our full changeset value
- (map
-  (fn [changes]
-    {:changes (into
-               {}
-               (map
-                (fn [[tx attr]]
-                  (let [tx-before-db (d/as-of db (dec (d/tx->t tx)))
-                        tx-after-db (d/as-of db tx)
-                        tx-e (d/entity tx-after-db tx)
-                        attr-e-before (d/entity tx-before-db attr)
-                        attr-e-after (d/entity tx-after-db attr)]
-                    [(:db/ident attr-e-after)
-                     {:old (get
-                            (d/entity tx-before-db entity-id)
-                            (:db/ident attr-e-before))
-                      :new (get
-                            (d/entity tx-after-db entity-id)
-                            (:db/ident attr-e-after))}]))
-                changes))
-     :timestamp (->> (ffirst changes)
-                     (d/entity (d/as-of db (ffirst changes)))
-                     :db/txInstant)})))
+(util/changeset-for entity-id db)
+
+
+
+
---
+++ src/datomic_event_sourcing/util.clj
@@ -0,0 +1,54 @@
+(ns datomic-event-sourcing.util
+  (:require [datomic.api :as d]))
+
+
+(defn get-inserted-entity
+  [tempid tx-res]
+  (d/entity (:db-after tx-res) (d/resolve-tempid (:db-after tx-res) (:tempids tx-res) tempid)))
+
+(defn transact-entity
+  [conn tempid tx]
+  (get-inserted-entity tempid @(d/transact conn tx)))
+
+
+(defn changeset-for [entity-id db]
+  (->>
+    ;; This query finds all tuples of the tx and the actual attribute that
+    ;; changed for a specific entity.
+ (d/q
+  '[:find ?tx ?a
+    :in $ ?e
+    :where
+    [?e ?a _ ?tx]]
+  (d/history db)
+  entity-id)
+ ;; We group the tuples by tx - a tx single can and will contain multiple
+ ;; attribute changes.
+ (group-by (fn [[tx attr]] tx))
+ ;; We only want the actual changes
+ (vals)
+ ;; Sort with oldest first
+ (sort-by (fn [[tx attr]] tx))
+ ;; Generate our full changeset value
+ (map
+  (fn [changes]
+    {:changes (into
+               {}
+               (map
+                (fn [[tx attr]]
+                  (let [tx-before-db (d/as-of db (dec (d/tx->t tx)))
+                        tx-after-db (d/as-of db tx)
+                        tx-e (d/entity tx-after-db tx)
+                        attr-e-before (d/entity tx-before-db attr)
+                        attr-e-after (d/entity tx-after-db attr)]
+                    [(:db/ident attr-e-after)
+                     {:old (get
+                            (d/entity tx-before-db entity-id)
+                            (:db/ident attr-e-before))
+                      :new (get
+                            (d/entity tx-after-db entity-id)
+                            (:db/ident attr-e-after))}]))
+                changes))
+     :timestamp (->> (ffirst changes)
+                     (d/entity (d/as-of db (ffirst changes)))
+                     :db/txInstant)}))))
